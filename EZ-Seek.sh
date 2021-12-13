4c10ec21488773a683270a1bd6abe5b0991e38b1
Keyz <number28@protonmail.com>
2021-12-13 12:20:07 -0500

    Comment Fix
    Fixed comments

--- EZ-Seek.sh
+++ EZ-Seek.sh
@@ -8,12 +8,12 @@
 # SIMPLE ELI5 
 ####################
 #
-# They payloads being sent will trigger DNS lookups. You can review your DNS logs to see if the Log4J module forwarded the request.
+# The payloads being sent will trigger DNS lookups. You can review your DNS logs to see if the Log4J module forwarded the request.
 # If you do not have your own server, you can use the publiclly availbe resources which this script is set to use. (dnslog.cn and log4shell.huntress.com)
 # 
 # Obtain a dns subdomain from dnslog.cn and fill in the DOMAIN variable with your unique address. Do the same for log4shell.huntress.com
 # 
-# The script will attempt to inject all of the DNS payloads into each HEADER, for every IP listed, to see if you it gets any hits. 
+# The script will attempt to inject all of the DNS payloads into each HEADER, for every IP listed to see if you it gets any hits. 
 # Consider the fact that your IP may be blocked if there is a WAF. So consider running this from behind a VPN or a proxy.
 #
 #####################
@@ -26,8 +26,12 @@
 # 2) Generate a subdomain at dnslog.cn and place it in the DOMAIN variable. 
 # 3) Replace the HUNTRESS LDAP payload variable with your assigned UUID at log4shell.huntress.com
 # 4) Because I did not include logic to check for SSL IPs, you must seperate SSL address and NONSSL addresses into two different files.
+#     -- SSL_FILE should contain ports if not 443, e.g. 127.0.0.1:8443
+#     -- NOSSL_FILE should contain ports if not port 80, e.g. 127.0.0.1:8080
+#     -- Each IP address should be on its own line
 #
 # LOGGING: If you would like a copy of the output, please run the command with tee, e.g. ./scan.sh | tee -a output.txt
+#
 ###################### 
 # VARIABLES
 ######################
